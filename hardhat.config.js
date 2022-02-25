b1a6dde821854eecb7ecd94c8ad10889352dc5a9
Damon Malkiewicz <damon@newexpanse.com>
2022-02-25 17:22:54 -0500

    Connect app to contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -21,8 +21,11 @@ module.exports = {
   solidity: '0.8.4',
   networks: {
     rinkeby: {
-      url: process.env.STAGING_ALCHEMY_KEY,
-      accounts: [process.env.PRIVATE_KEY],
+      url: process.env.REACT_APP_ALCHEMY_KEY,
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
     },
+  },
+  paths: {
+    artifacts: './src/artifacts',
   },
 };
--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
 import twitterLogo from './assets/twitter-logo.svg';
 import './styles/App.css';
+
+import myEpicNft from './artifacts/contracts/MyEpicNFT.sol/MyEpicNFT.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -59,6 +62,32 @@ const App = () => {
     }
   };
 
+  const askContractToMintNft = async () => {
+    const CONTRACT_ADDRESS = process.env.REACT_APP_CONTRACT_ADDR;
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log('Going to pop wallet now to pay gas...');
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+
+        console.log('Mining...please wait.');
+        await nftTxn.wait();
+
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
@@ -82,7 +111,8 @@ const App = () => {
           {currentAccount === '' ? (
             renderNotConnectedContainer()
           ) : (
-            <button onClick={null} className="cta-button connect-wallet-button">
+            /** Add askContractToMintNft Action for the onClick event **/
+            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
             </button>
           )}
